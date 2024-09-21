d77d4c0b86caaea5d96ca0e681c279fd645a7eee
Sk Mahammad Irfan <109087483+Sk8Mahammad-Irfan9@users.noreply.github.com>
2024-09-21 21:33:41 +0530

    styling changes

--- foodappp/src/pages/Checkout.js
+++ foodappp/src/pages/Checkout.js
@@ -1,19 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "../css/checkout.css";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, NavLink } from "react-router-dom";
 import useCartStore from "../store/cartStore";
 import axios from "axios";
 import { RiDeleteBin6Line } from "react-icons/ri";
-import { useStateHook } from "../store/cartStore";
-import toast from "react-hot-toast";
-import { Link, NavLink } from "react-router-dom";
-import MenuItem from "@mui/material/MenuItem";
 import { useAuth } from "./context/Auth/auth";
-import "../css/checkout.css";
+import toast from "react-hot-toast";
+import MenuItem from "@mui/material/MenuItem";
 
 const Checkout = () => {
-  document.title = "Chekout";
-  const { removeOneItemCart, addToCart, clearCart } = useStateHook();
+  document.title = "Checkout";
+
+  const { removeOneItemCart, addToCart, clearCart } = useCartStore();
   const [auth, setAuth] = useAuth();
   const navigate = useNavigate();
 
@@ -27,7 +25,6 @@ const Checkout = () => {
 
   function groupAndCountObjects(array) {
     const groupedObjects = {};
-
     array.forEach((item) => {
       const key = JSON.stringify(item.title);
       if (!groupedObjects[key]) {
@@ -39,57 +36,55 @@ const Checkout = () => {
     return Object.values(groupedObjects);
   }
 
-  const totalPrice = cartItems.reduce((total, item) => total + item.price, 0);
+  const totalPrice = cartItem.reduce(
+    (acc, item) => acc + item.price * item.count,
+    0
+  );
 
   if (cartItems.length === 0) {
     return (
-      <>
-        <div className="flex justify-center align-items-center text-2xl sm:max-h-max p-10 m-10">
-          <code>You Dont Have any items Go To ->></code>
-          <NavLink to="/menu" className="bg-orange-400">
-            Menu
-          </NavLink>
-        </div>
-      </>
+      <div className="flex justify-center items-center text-2xl sm:max-h-max p-10 m-10">
+        <code>You don't have any items. Go to ->></code>
+        <NavLink to="/menu" className="bg-orange-400 p-2 rounded ml-2">
+          Menu
+        </NavLink>
+      </div>
     );
   }
 
   const checkout = async () => {
     const orderData = {
-      items: cartItems.map((itemName) => itemName.title),
-      totalPrice: totalPrice,
+      items: cartItems.map((item) => item.title),
+      totalPrice,
       quantity: cartItems.length,
       userEmail: auth?.user?.email,
       orderNo: auth?.user?.orderNo,
     };
-    toast.loading("Please Wait!... Order details sending to your email");
-    await axios
-      .post("http://localhost:4000/api/create-order", orderData, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
 
-      .then((response) => {
-        const orderNumber = response.data.orderNo;
-        toast.dismiss();
-        clearCart();
-        navigate("/");
-        toast.success(`Order Placed ! Order Number : ${orderNumber}`);
-      })
-      .catch((err) => {
-        console.log("error placing order", err);
-      });
+    toast.loading("Please Wait! Order details sending to your email...");
+
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_API}/api/create-order`,
+        orderData,
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      const orderNumber = response.data.orderNo;
+      toast.dismiss();
+      clearCart();
+      navigate("/");
+      toast.success(`Order Placed! Order Number: ${orderNumber}`);
+    } catch (error) {
+      console.error("Error placing order", error);
+    }
   };
 
   const handleLogout = () => {
-    setAuth({
-      ...auth,
-      user: null,
-      token: "",
-    });
+    setAuth({ ...auth, user: null, token: "" });
     localStorage.removeItem("auth");
-    toast.success("Logout Successfully");
+    toast.success("Logged out successfully");
     navigate("/");
     window.location.reload();
   };
@@ -98,89 +93,104 @@ const Checkout = () => {
     <>
       <div className="list-none">
         <li>
-          <NavLink to="/menu" className="text-2xl">
+          <NavLink to="/menu" className="text-2xl text-black">
             &larr; Go Back
           </NavLink>
         </li>
       </div>
 
-      {!auth.user ? (
-        <></>
-      ) : (
-        <>
-          <div className="flex justify-around">
-            <MenuItem value="">
-              Hello : <em>{auth?.user?.name}</em>
-            </MenuItem>
-            <MenuItem value={20}>
-              <NavLink onClick={handleLogout} className="logout">
-                Log Out
-              </NavLink>
-            </MenuItem>
-          </div>
-        </>
+      {auth.user && (
+        <div className="flex justify-between items-center mb-2 text-white p-2 bg-gray-700 ">
+          <MenuItem value="">
+            Hello: <em>{auth?.user?.name}</em>
+          </MenuItem>
+          <MenuItem value={20}>
+            <NavLink
+              onClick={handleLogout}
+              className="logout text-red-400 hover:text-red-300"
+            >
+              Log Out
+            </NavLink>
+          </MenuItem>
+        </div>
       )}
 
-      <div className="checkOuts">
-        <ul>
+      <div className="checkOuts p-4 bg-gray-800 shadow-lg">
+        <ul className="list-none">
           {cartItem.map((item) => (
-            <li key={item.id}>
-              <div key={item.id} className="checkout-products">
-                <img
-                  src={item.img}
-                  style={{ height: "100px", width: "100px" }}
-                  alt="..."
-                />
-                <div></div>
-                <h3>{item.title}</h3>
-                <span>${item.price}</span>
-                <span>Sub Total -</span>
-                <span>${item.count * item.price}</span>
+            <li key={item.id} className="mb-4">
+              <div className="checkout-products bg-gray-700 p-4 flex items-center justify-between shadow-md hover:shadow-lg transition-shadow duration-300">
+                <div className="flex items-center">
+                  <img
+                    src={item.img}
+                    alt={item.title}
+                    className="h-24 w-24 object-cover rounded-md"
+                  />
+                  <div className="ml-4 text-left">
+                    <h3 className="text-white font-semibold">{item.title}</h3>
+                  </div>
+                </div>
+                <div className="text-white text-center mx-4">
+                  <span className="block">${item.price.toFixed(2)}</span>
+                  <span className="text-gray-500">Per Unit</span>
+                </div>
+                <div className="text-white text-center mx-4">
+                  <span className="block">
+                    ${(item.count * item.price).toFixed(2)}
+                  </span>
+                  <span className="text-gray-500">Subtotal</span>
+                </div>
                 <span
                   onClick={() =>
                     useCartStore.getState().removefromcart(item.id)
                   }
+                  className="text-red-500 cursor-pointer hover:text-red-300"
                 >
-                  <RiDeleteBin6Line color="red" cursor="pointer" />
+                  <RiDeleteBin6Line size={24} />
                 </span>
               </div>
-              <button className="add-item-list" onClick={() => addToCart(item)}>
-                +
-              </button>
-              <span className="item-count">{item.count}</span>
-              <button
-                className="remove-item-list"
-                onClick={() => removeOneItemCart(item)}
-              >
-                -
-              </button>
+              <div className="flex justify-center mt-2">
+                <button
+                  className="add-item-list rounded-full"
+                  onClick={() => addToCart(item)}
+                >
+                  +
+                </button>
+                <span className="item-count mx-4 text-lg">{item.count}</span>
+                <button
+                  className="remove-item-list rounded-full"
+                  onClick={() => removeOneItemCart(item)}
+                >
+                  -
+                </button>
+              </div>
             </li>
           ))}
         </ul>
       </div>
 
-      <hr />
+      <hr className="my-4 border-gray-600" />
+
       <div className="text-center">
         {auth?.token ? (
-          <>
-            <div className="cart-totals">
-              <h2>Cart Total</h2>
-              <h4>Total Amount: ${totalPrice}</h4>
-              <div>
-                <button className="checkout-btn" onClick={checkout}>
-                  Checkout
-                </button>
-              </div>
-            </div>
-          </>
+          <div className="cart-totals text-white mb-4">
+            <h2 className="text-lg font-bold">Cart Summary</h2>
+            <h4 className="text-xl">
+              Total Products:{" "}
+              {cartItem.reduce((acc, item) => acc + item.count, 0)}
+            </h4>
+            <h4 className="text-xl">Total Amount: ${totalPrice.toFixed(2)}</h4>
+            <button
+              className="checkout-btn bg-orange-600 hover:bg-orange-700 text-white py-2 px-4 rounded-lg transition duration-300"
+              onClick={checkout}
+            >
+              Checkout
+            </button>
+          </div>
         ) : (
           <button
-            className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold m-8 rounded"
-            onClick={() =>
-              navigate("/login", {
-                state: "/checkout",
-              })
-            }
+            className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold m-8 rounded-lg py-2 px-4 transition duration-300"
+            onClick={() => navigate("/login", { state: "/checkout" })}
           >
             Please register for Checkout
           </button>
